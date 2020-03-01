a5a4ce3e2479368d3799dcdfa549f8da425066b9
Postal06 <ohwdinfo@gmail.com>
2020-03-01 11:19:49 -0800

    made modals accessible, fixed scroll position reset when modal is clicked open

--- app.js
+++ app.js
@@ -45,97 +45,185 @@ let toggleNav = function () {
 
 /* Modals collected in variables */
 
-var $modalHome = $(".modal-one");
-var $modalNewHome = $(".modal-two");
-var $modalRoom = $(".modal-three");
-var $modalStairs = $(".modal-four");
-var $modalVictorian2 = $(".modal-five");
-var $modalVictorian4 = $(".modal-six");
-var $modalVictorian = $(".modal-seven");
-var $modalWhite = $(".modal-eight");
-var $modalVictorian3 = $(".modal-nine");
-var $modalRoom3 = $(".modal-ten");
+var $modalOne = $(".modal-one");
+var $modalTwo = $(".modal-two");
+var $modalThree = $(".modal-three");
+var $modalFour = $(".modal-four");
+var $modalFive = $(".modal-five");
+var $modalSix = $(".modal-six");
+var $modalSeven = $(".modal-seven");
+var $modalEight = $(".modal-eight");
+var $modalNine = $(".modal-nine");
+var $modalTen = $(".modal-ten");
+var $modalEleven = $(".modal-eleven");
+var $modalTwelve = $(".modal-twelve");
+var $modalThirteen = $(".modal-thirteen");
+var $modalFourteen = $(".modal-fourteen");
+var $modalFifteen = $(".modal-fifteen");
+var $modalSixteen = $(".modal-sixteen");
+var $modalSeventeen = $(".modal-seventeen");
+var $modalEighteen = $(".modal-eighteen");
+var $modalNineteen = $(".modal-nineteen");
+var $modalTwenty = $(".modal-twenty");
+var $modalTwentyOne = $(".modal-twenty-one");
 
 /* Images collected in variables */
 
 var $homeIcon = $(".home-icon");
 var $html = $("html");
 
-var $homec = $("#one");
-var $newHome = $("#two");
-var $room = $("#three");
-var $stairs = $("#four");
-var $victorian2 = $("#five");
-var $victorian4 = $("#six");
-var $victorian = $("#seven");
-var $white = $("#eight");
-var $victorian3 = $("#nine");
-var $room3 = $("#ten");
+var $one = $("#one");
+var $two = $("#two");
+var $three = $("#three");
+var $four = $("#four");
+var $five = $("#five");
+var $six = $("#six");
+var $seven = $("#seven");
+var $eight = $("#eight");
+var $nine = $("#nine");
+var $ten = $("#ten");
+var $eleven = $("#eleven");
+var $twelve = $("#twelve");
+var $thirteen = $("#thirteen");
+var $fourteen = $("#fourteen");
+var $fifteen = $("#fifteen");
+var $sixteen = $("#sixteen");
+var $seventeen = $("#seventeen");
+var $eighteen = $("#eighteen");
+var $nineteen = $("#nineteen");
+var $twenty = $("#twenty");
+var $twentyOne = $("#twenty-one");
 
 /* Add Clicked Class */
 
-$homec.on("click", function(e) {
-  $modalHome.addClass("clicked");
+$one.on("click", function(e) {
+  $modalOne.addClass("clicked");
   $html.addClass("clicked");
 });
 
-$newHome.on("click", function(e) {
-  $modalNewHome.addClass("clicked");
+$two.on("click", function(e) {
+  $modalTwo.addClass("clicked");
   $html.addClass("clicked");
 });
 
-$room.on("click", function(e) {
-  $modalRoom.addClass("clicked");
+$three.on("click", function(e) {
+  $modalThree.addClass("clicked");
   $html.addClass("clicked");
 });
 
-$stairs.on("click", function(e) {
-  $modalStairs.addClass("clicked");
+$four.on("click", function(e) {
+  $modalFour.addClass("clicked");
   $html.addClass("clicked");
 });
 
-$victorian2.on("click", function(e) {
-  $modalVictorian2.addClass("clicked");
+$five.on("click", function(e) {
+  $modalFive.addClass("clicked");
   $html.addClass("clicked");
 });
 
-$victorian4.on("click", function(e) {
-  $modalVictorian4.addClass("clicked");
+$six.on("click", function(e) {
+  $modalSix.addClass("clicked");
   $html.addClass("clicked");
 });
 
-$victorian.on("click", function(e) {
-  $modalVictorian.addClass("clicked");
+$seven.on("click", function(e) {
+  $modalSeven.addClass("clicked");
   $html.addClass("clicked");
 });
 
-$white.on("click", function(e) {
-  $modalWhite.addClass("clicked");
+$eight.on("click", function(e) {
+  $modalEight.addClass("clicked");
   $html.addClass("clicked");
 });
 
-$victorian3.on("click", function(e) {
-  $modalVictorian3.addClass("clicked");
+$nine.on("click", function(e) {
+  $modalNine.addClass("clicked");
   $html.addClass("clicked");
 });
 
-$room3.on("click", function(e) {
-  $modalRoom3.addClass("clicked");
+$ten.on("click", function(e) {
+  $modalTen.addClass("clicked");
+  $html.addClass("clicked");
+});
+
+$eleven.on("click", function(e) {
+  $modalEleven.addClass("clicked");
+  $html.addClass("clicked");
+});
+
+$twelve.on("click", function(e) {
+  $modalTwelve.addClass("clicked");
+  $html.addClass("clicked");
+});
+
+$thirteen.on("click", function(e) {
+  $modalThirteen.addClass("clicked");
+  $html.addClass("clicked");
+});
+
+$fourteen.on("click", function(e) {
+  $modalFourteen.addClass("clicked");
+  $html.addClass("clicked");
+});
+
+$fifteen.on("click", function(e) {
+  $modalFifteen.addClass("clicked");
+  $html.addClass("clicked");
+});
+
+$sixteen.on("click", function(e) {
+  $modalSixteen.addClass("clicked");
+  $html.addClass("clicked");
+});
+
+$seventeen.on("click", function(e) {
+  $modalSeventeen.addClass("clicked");
+  $html.addClass("clicked");
+});
+
+$eighteen.on("click", function(e) {
+  $modalEighteen.addClass("clicked");
+  $html.addClass("clicked");
+});
+
+$nineteen.on("click", function(e) {
+  $modalNineteen.addClass("clicked");
+  $html.addClass("clicked");
+});
+
+$twenty.on("click", function(e) {
+  $modalTwenty.addClass("clicked");
+  $html.addClass("clicked");
+});
+
+$twentyOne.on("click", function(e) {
+  $modalTwentyOne.addClass("clicked");
   $html.addClass("clicked");
 });
 
 /* Remove Clicked Class */
 
 $homeIcon.on("click", function(e) {
-  $modalHome.removeClass("clicked");
-  $modalNewHome.removeClass("clicked");
-  $modalRoom.removeClass("clicked");
-  $modalStairs.removeClass("clicked");
-  $modalVictorian2.removeClass("clicked");
-  $modalVictorian4.removeClass("clicked");
-  $modalVictorian.removeClass("clicked");
-  $modalWhite.removeClass("clicked");
-  $modalVictorian3.removeClass("clicked");
-  $modalRoom3.removeClass("clicked");
+  $modalOne.removeClass("clicked");
+  $modalTwo.removeClass("clicked");
+  $modalThree.removeClass("clicked");
+  $modalFour.removeClass("clicked");
+  $modalFive.removeClass("clicked");
+  $modalSix.removeClass("clicked");
+  $modalSeven.removeClass("clicked");
+  $modalEight.removeClass("clicked");
+  $modalNine.removeClass("clicked");
+  $modalTen.removeClass("clicked");
+  $modalEleven.removeClass("clicked");
+  $modalTwelve.removeClass("clicked");
+  $modalThirteen.removeClass("clicked");
+  $modalFourteen.removeClass("clicked");
+  $modalFifteen.removeClass("clicked");
+  $modalSixteen.removeClass("clicked");
+  $modalSeventeen.removeClass("clicked");
+  $modalEighteen.removeClass("clicked");
+  $modalNineteen.removeClass("clicked");
+  $modalTwenty.removeClass("clicked");
+  $modalTwentyOne.removeClass("clicked");
   $html.removeClass("clicked");
 });
